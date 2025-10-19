4b2858017d68bf85c954871d8d25a065b4295f9c
VIVEK <99870677+Roninopp@users.noreply.github.com>
2025-10-19 14:07:24 +0530

    Update logs.py

--- logs.py
+++ logs.py
@@ -76,6 +76,6 @@ def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
-    logger.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+    logger.critical(f"Uncaught exception: {exc_type.__name__}: {exc_value}")
 
 sys.excepthook = handle_exception
